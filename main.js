26ecbc812108afe9feb6624e54596d636fd79718
Christopher Carlsson <christopher@meetio.com>
2021-01-09 22:18:57 +0100

    Fixed reversal bug.

--- main.js
+++ main.js
@@ -358,7 +358,7 @@ var vm = new Vue({
       const dataset = [];
 
       let previousR = 0;
-      this.list.reverse().forEach((trade) => {
+      this.list.slice().reverse().forEach((trade) => {
         const r = trade.result / trade.stop;
         const sum = _.round(previousR + r, 2);
         dataset.push(sum);
